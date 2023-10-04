d3bd654571bf5db9ae00385faccfad203c0d74be
Evy <evytchr@gmail.com>
2023-10-04 21:30:27 +0200

    finalisation du design de la modale

--- FrontEnd/galery.js
+++ FrontEnd/galery.js
@@ -6,18 +6,29 @@ function displayWorks(works) {
     for (let i=0; i < works.length; i++) {
 
         const figure = works[i]
-        const gallery = document.querySelector(".gallery")
+        let galleries = document.querySelectorAll(".jsGallery")
+        console.log(galleries)
+
+        galleries.forEach(div => { 
         const workElement = document.createElement("figure")
+        div.appendChild(workElement)
 
         const imageElement = document.createElement("img")
         imageElement.src = figure.imageUrl
-
-        const captionElement = document.createElement("figcaption")
-        captionElement.innerText = figure.title
-
-        gallery.appendChild(workElement)
         workElement.appendChild(imageElement)
-        workElement.appendChild(captionElement)
+
+        if (div.classList.contains("gallery")) {
+            const captionElement = document.createElement("figcaption")
+            captionElement.innerText = figure.title
+            workElement.appendChild(captionElement)
+        } else {
+            const deleteButton = document.createElement("button")
+            const trash = document.createElement("i")
+            trash.classList.add("fa-solid", "fa-trash-can", "fa-xs")
+            workElement.appendChild(deleteButton)
+            deleteButton.appendChild(trash)
+        }
+        })
     }
 }
 
