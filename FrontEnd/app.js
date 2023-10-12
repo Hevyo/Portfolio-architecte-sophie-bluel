ee4dc03a4c8d3dfdb9eca2209eeeb126f9ea9819
Evy <evytchr@gmail.com>
2023-10-12 15:12:30 +0200

    Intégration de vérification du form ajout travaux

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -15,7 +15,7 @@ const openModal = function (e) {
     modal.removeAttribute("aria-hidden")
     modal.setAttribute("aria-modal", "true")
     modal.addEventListener("click", closeModal)
-    modal.querySelector(".jsCloseModal").addEventListener("click", closeModal)
+    modal.querySelectorAll(".jsCloseModal").forEach (x => { x.addEventListener("click", closeModal)})
     modal.querySelector(".jsModalStop").addEventListener("click", stopPropagation)
 }
 
@@ -30,8 +30,9 @@ const closeModal = function (e) {
     modal.setAttribute("aria-hidden" , true)
     modal.removeAttribute("aria-modal")
     modal.removeEventListener("click", closeModal)
-    modal.querySelector(".jsCloseModal").removeEventListener("click", closeModal)
-    modal.querySelector(".jsCloseModal").removeEventListener("click", stopPropagation)
+    modal.querySelectorAll(".jsCloseModal").forEach (x => { x.removeEventListener("click", closeModal)})
+    modal.querySelectorAll(".jsCloseModal").forEach (x => { x.removeEventListener("click", stopPropagation)})
+    clearForm()
 }
 
 const stopPropagation = function (e) {
@@ -55,7 +56,7 @@ const focusInModal = function(e) {
     focusables[index].focus()
 }
 
-// Non utilisé pour l'instant, en prévision de la création de plusieurs boutons faisant apparaitre la modale
+// Ouverture de la modale au clic sur le lien, forEach non utilisé pour l'instant (en prévision de la création de plusieurs boutons faisant apparaitre la modale)
 
 document.querySelectorAll(".jsModal").forEach(a => {
     a.addEventListener ("click" , openModal)
@@ -72,13 +73,21 @@ window.addEventListener("keydown", function (e) {
     }
 })
 
-// Remplacement du contenu de la modale pour l'ajout de travaux
+// Fonctionnalité de changement de page de modale et retour arrière
 
 const buttonAdWork = document.querySelector(".buttonAddWork")
 
 buttonAdWork.addEventListener("click", function () {
     document.querySelector(".modalPageOne").style.display = "none"
     document.querySelector(".modalPageTwo").style.display = null
+})
+
+const backToPreviousPage = document.querySelector(".jsBackToPreviousPage")
+
+backToPreviousPage.addEventListener("click", function () {
+    document.querySelector(".modalPageOne").style.display = null
+    document.querySelector(".modalPageTwo").style.display = "none"
+    clearForm()
 })
 
 
@@ -91,3 +100,93 @@ for (let i=0; i < category.length; i++) {
     optionToSelect.value , optionToSelect.innerText = category[i].name
     selectCategory.appendChild(optionToSelect)
 }
+
+// Redirection vers l'input file masqué au clic sur le boutton stylisé "+ ajouter photo"
+
+const jsRedirectInputFile = document.querySelector(".jsRedirectInputFile")
+
+jsRedirectInputFile.addEventListener("click", function () {
+    document.querySelector(".jsInputFile").click()
+})
+
+// Prévisualisation de l'image
+
+const inputFile = document.querySelector(".jsInputFile")
+const previewImage = document.getElementById('previewImage')
+const modalErrorMessage = document.querySelector(".modalErrorMessage")
+
+
+
+inputFile.addEventListener('change', function (e) {
+    console.log(e.target.files[0])
+    if (validFileType(e.target.files[0])) {
+        previewImage.src = URL.createObjectURL(e.target.files[0])
+        previewImage.style.display = "block"
+        jsRedirectInputFile.style.visibility = "hidden"
+        modalErrorMessage.style.display = "none"
+    } else {
+        inputFile.value = ""
+        console.log(inputFile.value)
+        if (modalErrorMessage.style.display = "none") {
+            modalErrorMessage.style.display = null
+            modalErrorMessage.innerText = "Type de fichier invalide"
+        }
+    }
+})
+
+// Vérification de l'extension de l'image uploadée
+
+const fileTypes = ["image/jpg" , "image/png"]
+
+
+function validFileType(file) {
+    
+    for (let i = 0; i < fileTypes.length; i++) {
+        if (file.type === fileTypes[i]) {
+            return true
+        }
+    }
+    return false
+}
+
+// Vérification des champs pour l'envoi du formulaire 
+
+const inputTitleWork = document.getElementById("title")
+const buttonSubmitWork = document.querySelector(".buttonSubmitWork")
+const workUploadForm = document.querySelector(".workUploadForm")
+
+        // Fonction qui vérifie si les champs sont remplis et active le bouton d'envoi du formulaire
+
+function validateForm() {
+    let categorySelected = workUploadForm.category.options[workUploadForm.category.options.selectedIndex].value;
+    console.log(inputFile.value + " " +  categorySelected + " " + inputTitleWork.value)
+    if (inputFile.value && categorySelected && inputTitleWork.value !== "" ) {
+        buttonSubmitWork.disabled = false
+        modalErrorMessage.style.display = "none"
+    } else {
+        modalErrorMessage.style.display = null
+        modalErrorMessage.innerText = "Veuillez remplir tous les champs"
+    }
+}
+
+        // Utilisation de la fonction de vérification pour chaque élément input au changement
+
+const allInputsForm = [inputFile , workUploadForm , inputTitleWork]
+console.log(allInputsForm)
+
+allInputsForm.forEach(input => {
+    input.addEventListener("change", function(e) {
+    validateForm(e.target)
+    console.log(e.target)
+    })
+})
+
+// Fonction qui réinitialise le formulaire, utilisée à la fermeture de la modale et au changement de page de modale
+
+function clearForm() {
+    workUploadForm.reset()
+    previewImage.style.display = "none"
+    buttonSubmitWork.disabled = true
+    jsRedirectInputFile.style.visibility = "visible"
+}
+
